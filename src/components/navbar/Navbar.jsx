e273e8d78bab75fd64e95630921630d89b12aa8b
Niranjan <niranjanhebli77@gmail.com>
2023-02-14 19:21:38 +0530

    Logo added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import { Rjmenu3Line, RjcloseLine } from "react-icons/ri";
+import "./navbar.css";
+import logo from '../../assets/logo.svg';
+//BEM-> Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+      </div>
+    </div>  );
+};
 
 export default Navbar;
