91c1b12fcf2f2e4be75be42961280c3cac39d4f4
Niranjan <niranjanhebli77@gmail.com>
2023-02-21 00:40:28 +0530

    Blog started

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({imgURL}) => {
   return (
-    <div>
-      Article
-    </div>
+<div className="gpt3__blog-container_article">
+  <div className="gpt3__blog-container_article-img">
+    <img src ={imgURL} alt="blog image"/>
+  </div>
+</div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './blog.css'
+import Article from "../../components/article/Article";
+import { blog01,blog02,blog03,blog04,blog05 } from './import';
 
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">A lot is happening we are blogging about it</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgURL={blog01}/>
+
+        </div>
+        <div className="gpt3__blog-container_groupB">
+         <Article imgURL={blog02}/>
+          <Article imgURL={blog03}/>
+          <Article imgURL={blog04}/>
+          <Article imgURL={blog05}/>
+        </div>
+      </div>
+
     </div>
   )
 }
